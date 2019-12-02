70a06bc39c5dd252cc7be497f971f7cc34a6b5a5
562790012@qq.com <562790012@qq.com>
2019-12-02 23:20:13 +0800

    修改为spring boot kafka

---
+++ server/src/main/java/com/zhengpeidong/redis/server/kafka/SpringBootKafkaConsumerExampleApplication.java
@@ -0,0 +1,12 @@
+package com.zhengpeidong.redis.server.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaConsumerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKafkaConsumerExampleApplication.class, args);
+	}
+}
---
+++ server/src/main/java/com/zhengpeidong/redis/server/kafka/listener/KafkaConsumer.java
@@ -0,0 +1,21 @@
+package com.zhengpeidong.redis.server.kafka.listener;
+
+import com.zhengpeidong.redis.server.kafka.model.User;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+
+    @KafkaListener(topics = "Kafka_Example", containerGroup = "group_id")
+    public void consume(String message) {
+        System.out.println("Consumed message: " + message);
+    }
+
+
+    @KafkaListener(topics = "Kafka_Example_json", containerGroup = "group_json",
+            containerFactory = "userKafkaListenerFactory")
+    public void consumeJson(User user) {
+        System.out.println("Consumed JSON Message: " + user);
+    }
+}
